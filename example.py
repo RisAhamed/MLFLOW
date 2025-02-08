7d0e4697aa507dc6b79a670c80eba8143ca8265a
RisAhamed <riswanahamed38@gmail.com>
2025-02-08 09:28:09 +0530

    CI.Yaml Added and configured :first push fro CI

--- example.py
+++ example.py
@@ -75,17 +75,8 @@ if __name__ == "__main__":
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
 
-        
-        # # For remote server only (Dagshub)
-        remote_server_uri = "https://dagshub.com/RisAhamed/MLFLOW.mlflow"
-        mlflow.set_tracking_uri(remote_server_uri)
-
-
-        # # For remote server only (AWS)
-        # remote_server_uri = "http://ec2-54-147-36-34.compute-1.amazonaws.com:5000/"
-        # mlflow.set_tracking_uri(remote_server_uri)
-
-
+        # Set default MLflow tracking URI
+        mlflow.set_tracking_uri("http://localhost:5000")
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
